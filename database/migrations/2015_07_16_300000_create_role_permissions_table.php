063eba6c966af3ceebc6d3643a23f6172d94193a
Shirish <contactsneha21@gmail.com>
2015-07-29 12:06:12 +0530

    role management and minor changes added

---
+++ database/migrations/2015_07_16_300000_create_role_permissions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_permissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pid')->unsigned();
+            $table->integer('rid')->unsigned();
+            $table->foreign('pid')->references('pid')->on('permissions');
+            $table->foreign('rid')->references('rid')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('role_permissions');
+    }
+}
---
+++ src/RolePermissions.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Focalworks\Users;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RolePermissions extends Model
+{
+
+    public $timestamps = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['pid', 'rid'];
+
+    public function permissions()
+    {
+        return $this->hasOne('Focalworks\Users\Permissions', 'pid', 'pid');
+    }
+
+}
