fdecaa5aa9408b1192278e0153bb54822c361857
Shirish <contactsneha21@gmail.com>
2015-08-03 11:55:27 +0530

    seeder changes are done

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -8,91 +8,91 @@ class PermissionsTableSeeder extends Seeder
 {
     public function run()
     {
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'user_list',
                 'display_name' => 'Users List',
                 'group' => 'User'
             )
         ]);
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'view_user',
                 'display_name' => 'View User Profile',
                 'group' => 'User'
             )
         ]);
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'edit_user',
                 'display_name' => 'Edit User Profile',
                 'group' => 'User'
             )
         ]);
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'delete_user',
                 'display_name' => 'Delete User Profile',
                 'group' => 'User'
             )
         ]);
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'change_user_password',
                 'display_name' => 'Change User Password',
                 'group' => 'User'
             )
         ]);
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'myprofile',
                 'display_name' => 'My Profile',
                 'group' => 'User'
             )
         ]);
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'edit_profile',
                 'display_name' => 'Edit Profile',
                 'group' => 'User'
             )
         ]);
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'change_password',
                 'display_name' => 'Change Password',
                 'group' => 'User'
             )
         ]);
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'permission_matrix',
                 'display_name' => 'Permission Matrix',
                 'group' => 'Permission'
             )
         ]);
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'role_listing',
                 'display_name' => 'Role List',
                 'group' => 'Role'
             )
         ]);
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'add_role',
                 'display_name' => 'Add Role',
                 'group' => 'Role'
             )
         ]);
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'edit_role',
                 'display_name' => 'Edit Role',
                 'group' => 'Role'
             )
         ]);
-        \Focalworks\Users\UserRoles::create([
+        \Focalworks\Users\Permissions::create([
             array(
                 'name' => 'delete_role',
                 'display_name' => 'Delete Role',
