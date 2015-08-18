f88961609ea8d1ed4f4666cd943880823ef4d011
Amitav Roy <reachme@amitavroy.com>
2015-08-18 18:38:29 +0530

    Fixed the bootstrap class issue

--- views/partials/menubar.blade.php
+++ views/partials/menubar.blade.php
@@ -1,49 +1,51 @@
 @if(Auth::check())
-    <nav class="navbar navbar-default navbar-static">
-        <div class="container-fluid">
-            <ul class="nav navbar-nav">
-                <li><a href="{{ url('users/dashboard')  }}" class="btn btn-link">Dashboard</a></li>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <ul class="nav navbar-nav">
+            <a href="{{ url('users/dashboard')  }}" class="navbar-brand">Dashboard</a>
 
-                @if(access_check('manage_users',true) || access_check('manage_permisssion_matrix',true) || access_check('manage_permissions',true) || access_check('manage_roles',true))
-                    <li class="dropdown">
-                        <a href="#" id="navDropAdmin" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                           aria-haspopup="true" aria-expanded="false">Admin Functions<span class="caret"></span></a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDrop1">
-                            @if(access_check('manage_users',true))
-                                <li><a href="{{ url('admin/userListing') }}" class="btn btn-link">All Users</a></li>
-                            @endif
-                            @if(access_check('manage_roles',true))
-                                <li><a href="{{ url('admin/roleListing') }}" class="btn btn-link">All Roles</a></li>
-                            @endif
-                            @if(access_check('manage_permissions',true))
-                                <li><a href="{{ url('admin/permissionsListing')}}" class="btn btn-link">Permissions</a></li>
-                            @endif
-                            @if(access_check('manage_permission_matrix',true))
-                                <li class=""><a href="{{ url('admin/permissionMatrix')  }}" class="btn btn-link">Permission Matrix</a></li>
-                            @endif
-                        </ul>
+            @if(access_check('manage_users',true) || access_check('manage_permisssion_matrix',true) || access_check('manage_permissions',true) || access_check('manage_roles',true))
+            <li class="dropdown">
+                <a href="#" id="navDropAdmin" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                aria-haspopup="true" aria-expanded="false">Admin Functions<span class="caret"></span></a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDrop1">
+                    @if(access_check('manage_users',true))
+                    <li><a href="{{ url('admin/userListing') }}">All Users</a></li>
+                    @endif
+                    @if(access_check('manage_roles',true))
+                    <li><a href="{{ url('admin/roleListing') }}">All Roles</a></li>
+                    @endif
+                    @if(access_check('manage_permissions',true))
+                    <li><a href="{{ url('admin/permissionsListing')}}">Permissions</a>
                     </li>
+                    @endif
+                    @if(access_check('manage_permission_matrix',true))
+                    <li class=""><a href="{{ url('admin/permissionMatrix')  }}">Permission
+                        Matrix</a></li>
+                        @endif
+                    </ul>
+                </li>
                 @endif
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown pull-right">
                     <a href="#" id="navbarDrop1" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                       aria-haspopup="true" aria-expanded="false">
-                        <p class="text-info"><span class="glyphicon glyphicon-user"></span>
-                            Welcome {!! ucfirst(Auth::user()->name) !!} <span class="caret"></span></p></a>
+                    aria-haspopup="true" aria-expanded="false">
+                    <span class="glyphicon glyphicon-user"></span>
+                    Welcome {!! ucfirst(Auth::user()->name) !!} <span class="caret"></span></a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDrop1">
-                     @if(access_check('manage_profile',true))
-                        <li><a href="{{ url('users/myprofile')  }}" class="btn btn-link">My Profile</a></li>
-                        <li><a href="{{ url('users/editProfile')  }}" class="btn btn-link">Edit Profile</a></li>
-                        <li><a href="{{ url('users/changePassword')  }}" class="btn btn-link">Change Password</a>
+                        @if(access_check('manage_profile',true))
+                        <li><a href="{{ url('users/myprofile')  }}">My Profile</a></li>
+                        <li><a href="{{ url('users/editProfile')  }}">Edit Profile</a></li>
+                        <li><a href="{{ url('users/changePassword')  }}">Change Password</a>
                         </li>
-                     @endif
+                        @endif
                         <li role="separator" class="divider"></li>
-                        <li><a href="{{ url('users/logout')  }}" class="btn btn-link">Logout</a></li>
+                        <li><a href="{{ url('users/logout')  }}">Logout</a></li>
                     </ul>
                 </li>
             </ul>
         </div>
     </nav>
-@endif
+    @endif
